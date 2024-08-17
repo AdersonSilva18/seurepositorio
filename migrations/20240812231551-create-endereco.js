e8cab7c14591d7aa12a0df2b95485dd1d8250aee
renandgomes <nandelalibera@gmail.com>
2024-08-16 22:07:34 -0300

    wip

--- migrations/20240812231551-create-endereco.js
+++ migrations/20240812231551-create-endereco.js
@@ -1,21 +1,50 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Users', [
-      {
-       
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Enderecos", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: true,
       },
-    ]);
+      logradouro: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      numero: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      cep: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      complemento: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      bairro: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cidade: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      estado: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      municipioIBGE: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface, Sequelize) => {
+    await  queryInterface.dropTable("Enderecos");
+  },
 };
